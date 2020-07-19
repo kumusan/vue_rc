fd2c02e8a0f1d5ba3e2026dbe1d8f881369f7f33
kumusan <ryogi0200@gmail.com>
2020-07-19 23:12:27 +0900

    🍩Teleport

---
+++ src/components/TeleportText.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="target"></div>
+    <button @click="toggle">Button</button>
+    <!--err-->
+    <Teleport to="#target">
+      <div v-if="isBool">
+        <p>Teleport</p>
+        <button @click="toggle">Button</button>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref } from 'vue'
+export default defineComponent ({
+  setup() {
+    const isBool = ref(false)
+    const toggle = () => {
+      isBool.value = !isBool.value
+    }
+
+    return {
+      isBool,
+      toggle
+    }
+  }
+})
+</script>
