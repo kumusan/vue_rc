9ae4088a807e3b30a9747205f208799453af7584
kumusan <ryogi0200@gmail.com>
2020-07-19 23:10:30 +0900

    🍜init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <TeleportText />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Suspense>
+      <template #default>
+        <AsyncSuspens />
+      </template>
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script lang='ts'>
+import { ref ,defineComponent } from 'vue'
+import TeleportText from './components/TeleportText.vue'
+import AsyncSuspens from './components/AsyncSuspens.vue'
+
+export default defineComponent ({
+  name: 'App',
+  components: {
+    TeleportText,
+    AsyncSuspens
+  }, 
+  setup() {}
+})
+</script>
